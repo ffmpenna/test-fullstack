e3954659f27dfa07cea31890db7f49174a19eb27
Fabio Penna <ffmpenna@gmail.com>
2024-03-18 15:52:32 -0300

    feat: cria os arquivos que farão a validação dos dados dos clientes

---
+++ back-end/src/services/validations/schemas.ts
@@ -0,0 +1,27 @@
+import Joi from 'joi';
+
+const clientSchema = Joi.object({
+  name: Joi.string().min(3).label('name').required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'br', 'net', 'org', 'gov'] },
+    })
+    .label('email')
+    .required(),
+  phone: Joi.string()
+    .pattern(
+      /^(?:(?:(?:\+|00)?(55)\s?)?(?:(?:(?:[1-9][0-9])|(?:[1-9][0-9]{3}))\s?)?)(?:9\d{4}-?\d{4}|\d{4}-?\d{4})$/
+    )
+    .label('phone')
+    .required()
+    .messages({ 'string.pattern.base': 'Número de telefone inválido' }),
+  cpf: Joi.string()
+    .pattern(/^(\d{3}\.){2}\d{3}-\d{2}$|^\d{11}$/)
+    .label('cpf')
+    .required()
+    .messages({ 'string.pattern.base': 'Número de CPF inválido' }),
+  status: Joi.number().min(1).max(4).label('status').required(),
+});
+
+export default clientSchema;
---
+++ back-end/src/services/validations/validateClientData.ts
@@ -0,0 +1,48 @@
+import IClient from '../../Interfaces/IClient';
+import ClientsModel from '../../models/clients.model';
+import clientSchema from './schemas';
+
+const validateClientInputs = (data: IClient) => {
+  const { error } = clientSchema.validate(data);
+  if (error) {
+    const status = Object.values(data).some((e) => e === undefined) ? 400 : 422;
+    return { status, message: error.message };
+  }
+};
+
+const verifyExistingClient = async (id: number) => {
+  const clientsModel = new ClientsModel();
+  const existingClient = await clientsModel.getOne(id);
+  if (!existingClient) return { status: 400, message: 'Client does not exist' };
+};
+
+const verifyConflictingClientData = async (data: IClient, id?: number) => {
+  const clientsModel = new ClientsModel();
+  const conflictData = await clientsModel.getByEmailCpfOrPhone(
+    data.email,
+    data.cpf,
+    data.phone
+  );
+
+  if (conflictData && conflictData.id !== id) {
+    return { status: 400, message: 'Email, CPF, or phone already exists' };
+  }
+};
+
+const validateClient = async (data: IClient, id?: number) => {
+  const validationError = validateClientInputs(data);
+  if (validationError) return validationError;
+
+  const existingClientError = await verifyConflictingClientData(data, id);
+  if (existingClientError) return existingClientError;
+};
+
+const validateUptadeClient = async (data: IClient, id: number) => {
+  const existingClientError = await verifyExistingClient(id);
+  if (existingClientError) return existingClientError;
+
+  const validationError = await validateClient(data, id);
+  if (validationError) return validationError;
+};
+
+export { validateClient, validateUptadeClient, verifyExistingClient };
