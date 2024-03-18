9d61d25b6c019dce54fa9543ad7d9a029f7ab03b
Fabio Penna <ffmpenna@gmail.com>
2024-03-18 16:23:40 -0300

    feat: configura o main.tsx

--- teste-front-uol/src/main.jsx
+++ teste-front-uol/src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme/theme';
+import Provider from './context/Provider';
+import Header from './components/Header';
+import ClientFormPage from './pages/ClientFormPage';
+import ClientsPage from './pages/ClientsPage';
+import NotFoundPage from './pages/NotFoundPage';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <ClientsPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: '/clients',
+    element: <ClientsPage />,
+  },
+  {
+    path: 'clients/create',
+    element: <ClientFormPage page='create' />,
+  },
+  {
+    path: 'clients/edit/:id',
+    element: <ClientFormPage page='edit' />,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <Provider>
+        <Header />
+        <RouterProvider router={router} />
+      </Provider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
